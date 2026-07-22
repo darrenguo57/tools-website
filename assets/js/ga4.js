65e67631c28d8480aa3e6513f82a4e2e91ed791a
darrenguo57 <darrenguo57@gmail.com>
2026-07-22 17:15:43 +0800

    feat: add Google Analytics 4 tracking (G-4WFG5TSM6M) to all pages

---
+++ assets/js/ga4.js
@@ -0,0 +1,8 @@
+/**
+ * Google Analytics 4 - Global Site Tag (gtag.js)
+ * Measurement ID: G-4WFG5TSM6M
+ */
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+gtag('config', 'G-4WFG5TSM6M');
